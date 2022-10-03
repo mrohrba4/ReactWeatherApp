1decd93a86c419449ba14af5cd481dd7e2f09c6c
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2022-10-03 12:21:14 -0400

    Finish v1, formatted code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import { useState } from 'react';
-import Search from './components/search/search';
-import CurrentWeather from './components/current-weather/current-weather';
-import Forecast from './components/forecast/forecast';
-import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import "./App.css";
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 
 function App() {
-
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`);
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`);
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
@@ -22,7 +25,7 @@ function App() {
         const forecastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label,forecastResponse });
+        setForecast({ city: searchData.label, forecastResponse });
       })
       .catch((err) => console.log(err));
   };
