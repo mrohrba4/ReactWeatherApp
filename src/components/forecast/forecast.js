2f1fe870702d6477584acf04ff2b2db08df9b694
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2022-10-03 11:41:28 -0400

    Fix forecast api call error

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,11 +1,32 @@
-import { Accordion } from "react-accessible-accordion";
+import React from "react";
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 
-const Forecast = () => {
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+    console.log(data);
     return (
         <>
             <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
-                
+                {data.forecastResponse.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img src={`icons/${item.weather[0].icon}.png`} alt="weather" className="icon-small" />;
+                                    <label className="day">{forecastDays[idx]}</label>
+                                    <label className="description">{item.weather[0].description}</label>
+                                    <label className="min-max">{Math.round(item.main.temp_min)}°F / {Math.round(item.main.temp_max)}°F</label>
+
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
             </Accordion>
         </>
     );
